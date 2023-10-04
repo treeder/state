de354d3403270d1fd2279d97cb7b04e1cc66638f
Travis Reeder <treeder@gmail.com>
2023-10-03 17:28:49 -0700

    Create state.js

---
+++ state.js
@@ -0,0 +1,40 @@
+
+class State extends EventTarget { // implements EventTarget (partially anyways) https://developer.mozilla.org/en-US/docs/Web/API/EventTarget
+
+    constructor() {
+        super()
+
+        this.stateMap = new Map()
+    }
+
+    set(key, value) {
+        let m = this.stateMap.set(key, value)
+        this.dispatchEvent(new CustomEvent(key, {
+            detail: {
+                action: 'set',
+                key,
+                value,
+            },
+        }))
+        return m // can chain calls together
+    }
+
+    delete(key) {
+        let r = this.stateMap.delete(key)
+        this.dispatchEvent(new CustomEvent(key, {
+            detail: {
+                action: 'delete',
+                key,
+                deleted: r, // true if element existed and was removed, or false if the element did not exist.
+            },
+        }))
+    }
+
+    get(key) {
+        return this.stateMap.get(key)
+    }
+
+}
+
+const state = new State()
+export default state
